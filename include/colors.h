c4ef0e6675b49685280191342256aa08c7043400
Yiğit <yigitinstudyoo@gmail.com>
2026-03-09 21:07:51 +0300

    color file v1.0

---
+++ include/colors.h
@@ -0,0 +1,17 @@
+/*
+ * 2026 (c) Copyright - LibYGT Developers
+ * Log system made in C
+ * Licenced under the MIT license.
+ * For details, look at the LICENSE file.
+*/
+
+#ifndef COLORS_H
+#define COLORS_H
+
+#define LY_RED     "\033[31m" // red
+#define LY_YELLOW  "\033[33m" // yellow
+#define LY_GREEN   "\033[32m" // green
+#define LY_RESET   "\033[0m" // reset
+#define LY_CYAN    "\033[36m" // cyan
+
+#endif
