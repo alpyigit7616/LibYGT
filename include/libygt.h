bbcf20feaa36e90640e54f45cb14743b4d55165b
Yiğit <yigitinstudyoo@gmail.com>
2026-03-09 21:08:34 +0300

    Add libygt.h header file with logging functions

---
+++ include/libygt.h
@@ -0,0 +1,38 @@
+/*
+ * 2026 (c) Copyright - LibYGT Developers
+ * Log system made in C
+ * Licenced under the MIT license.
+ * For details, look at the LICENSE file.
+*/
+
+// Of course, the #... stuff
+#ifndef LIBYGT_H
+#define LIBYGT_H
+
+// Here, all functions...
+
+/*
+ ******************************
+ LOG FUNCTIONS
+ ******************************
+*/
+void ly_log_message(const char* message); // Normal message
+void ly_log_warn(const char* message); // Warning message
+void ly_log_error(const char* message); // Error message
+
+/*
+ ******************************
+ MSGBOX FUNCTIONS
+ ******************************
+*/
+void ly_msgbox_normal(const char *message); // Normal message
+void ly_msgbox_warn(const char *message); // Warning message
+void ly_msgbox_error(const char *message); // Error message
+
+typedef enum {
+    LY_INFO,
+    LY_WARN,
+    LY_ERROR
+} ly_log_level;
+
+#endif // End of the code
